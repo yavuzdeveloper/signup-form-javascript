abfc5065061afa5f4062dc9749da716a08675dfd
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-10-25 16:24:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function (e) {
+  e.preventDefault();
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  this.classList.toggle("bi-eye");
+});
+
+const submitBtn = document.querySelector("#submitBtn");
+const submit = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  document.getElementById("user-details").style.display = "none";
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(
+    () => (document.getElementById("spinner").style.display = "none"),
+    1000
+  );
+  setTimeout(
+    () => (document.getElementById("complete").style.display = "block"),
+    1000
+  );
+});
